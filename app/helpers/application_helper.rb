1160f34e68e18031549ca071e3bf10b05e0fde4b
Andy Shin <andrewkshin@gmail.com>
2010-12-15 21:51:28 -0800

    Revert "Revert "check in idea""
    This reverts commit a23fcf422107e6206c6690fc57cb5525dd14ce7f.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,17 @@
 module ApplicationHelper
   def replacing_content(&block)
-
-    raw("SCPZ.$('#scpz .content').html('#{escape_javascript(capture(&block))}');")
+    raw(serving_js do
+      "$('#scpz .content').html('#{escape_javascript(capture(&block))}');"
+    end)
   end
 
   def serving_js(&block)
-    raw(capture(&block))
+    raw(aliasing_jquery do
+      capture(&block)
+    end)
+  end
+
+  def aliasing_jquery(&block)
+    "(function($){#{capture(&block)}})(SCPZ.$);\n"
   end
 end
